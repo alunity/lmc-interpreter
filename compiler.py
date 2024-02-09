d3e3f0f4985dbc6e6a3e2fabdb1737cda28998fa
alunity <75143943+alunity@users.noreply.github.com>
2024-02-09 12:35:40 +0000

    Added token class

--- compiler.py
+++ compiler.py
@@ -1,4 +1,6 @@
-instructions = {
+from enum import Enum
+
+instructions: dict[str, str] = {
     "lda": "5xx",
     "sta": "3xx",
     "add": "1xx",
@@ -13,11 +15,46 @@ instructions = {
 }
 
 
-def tokenise(code: list[str]) -> list[list[str]]:
-    lines: list[list[str]] = []
+class TokenType(Enum):
+    LABEL = "label"
+    INSTRUCTION = "instruction"
+    OPERAND_LITERAL = "operand literal"
+    OPERAND_LABEL = "operand label"
+
+
+class Token:
+    def __init__(self, token_type: TokenType, value: str) -> None:
+        self.token_type: TokenType = token_type
+        self.value: str = value
+
+    def __str__(self) -> str:
+        return self.value
+
+
+def tokenise(code: list[str]) -> list[list[Token]]:
+    tokens: list[list[Token]] = []
     for i in code:
-        lines.append(i.split(" "))
-    return lines
+        parts: list[str] = i.split(" ")
+        line: list[Token] = []
+
+        # Label refers to the the name of a memory location
+        # When a label is used as a operand, it is categorised as an operand not a label
+        # Label can only be the first element therefore any element after the first element cannot be a label
+
+        for part in parts:
+            # Handle empty lines
+            if part != "":
+                if part in instructions:
+                    line.append(Token(TokenType("instruction"), part))
+                elif len(line) == 0:
+                    line.append(Token(TokenType("label"), part))
+                elif part.isdigit():
+                    line.append(Token(TokenType("operand literal"), part))
+                else:
+                    line.append(Token(TokenType("operand label"), part))
+        if len(line) > 0:
+            tokens.append(line)
+    return tokens
 
 
 def compile(code: list[str]) -> list[str]:
@@ -26,79 +63,76 @@ def compile(code: list[str]) -> list[str]:
     As specified here https://www.yorku.ca/sychen/research/LMC/LMCInstructionSummary.html
     """
 
-    lines: list[list[str]] = tokenise(code)
+    lines: list[list[Token]] = tokenise(code)
     #
     # Label table
     #
-
-    # Each line should either begin with a instruction or a label#
 
     # format(label: line number)
     label_table: dict[str, int] = {}
 
     for line_number, tokens in enumerate(lines):
-        if tokens[0] not in instructions:
-            label_table[tokens[0]] = line_number
+        # Each line should either begin with a instruction or a label
+        if tokens[0].token_type == TokenType.LABEL:
+            label_table[tokens[0].value] = line_number
 
     machine_code: list[str] = []
     for line_number, tokens in enumerate(lines):
+
         # Get instruction depending on whether first token is label or not
-        if tokens[0] in label_table:
-            instruction_index: int = 1
+        instruction: Token | None = None
+        operand: Token | None = None
+
+        if tokens[0].token_type == TokenType.LABEL:
+            instruction = tokens[1]
         else:
-            instruction_index: int = 0
-        instruction: str = tokens[instruction_index]
+            instruction = tokens[0]
 
-        #
-        # Error checking
-        #
+        if (
+            tokens[-1].token_type == TokenType.OPERAND_LABEL
+            or tokens[-1].token_type == TokenType.OPERAND_LITERAL
+        ):
+            operand = tokens[-1]
 
-        if instruction not in instructions:
-            raise Exception(f"Invalid instruction on line {line_number}: {tokens}")
+        if instruction.token_type != TokenType.INSTRUCTION:
+            raise SyntaxError(
+                f"Expected instruction on line {line_number}\n{' '.join(str(token) for token in tokens).upper()}"
+            )
 
-        # dat is an exception since it can have 1,2 or 3 tokens depending on whether it has no label and no value, no label or no value, a label and a value respectively
-        if instruction == "dat":
-            if len(tokens) > 3:
-                raise Exception(
-                    f"Line {line_number} has too many tokens: {' '.join(tokens).upper()}"
-                )
-        else:
-
-            # If machine code instruction has xx it expects an opcode therefore line should have 2-3 tokens depending on whether or not a label is present or not
-            if "xx" in instructions[instruction]:
-                expected = 2
-            else:
-                expected = 1
-            if len(tokens) != expected + instruction_index:
-                raise Exception(
-                    f"Line {line_number} has {'too many' if len(tokens) > expected + instruction_index else 'too few'} tokens: {' '.join(tokens).upper()}"
-                )
+        # Maximum tokens per line is 3
+        if len(tokens) > 3:
+            # Use list comprehension to allow for Tokens to be joined
+            raise SyntaxError(
+                f"Line {line_number} has too many tokens\n{' '.join(str(token) for token in tokens).upper()}"
+            )
 
         #
         # Code generation
         #
 
-        x: str = instructions[instruction]
+        x: str = instructions[instruction.value]
 
-        if instruction == "dat":
-            if len(tokens) == 1 + instruction_index:
-                x = "0"
-            else:
-                x = tokens[instruction_index + 1]
-                if x.isdigit():
-                    x = str(int(x))
+        if instruction.value == "dat":
+            if operand:
+                if (
+                    operand.value.isdigit()
+                    and operand.token_type == TokenType.OPERAND_LITERAL
+                ):
+                    x = str(int(operand.value))
                 else:
-                    raise Exception(
-                        f"Values in memory must be integers. Line {line_number}: {' '.join(tokens)}"
+                    raise SyntaxError(
+                        f"Expected integer. Line {line_number} \n{' '.join(str(token) for token in tokens).upper()}"
                     )
-
-        if "xx" in x:
-            operand = tokens[instruction_index + 1]
-            if operand.isnumeric():
-                operand_address = operand.zfill(2)
             else:
-                operand_address = str(label_table[operand]).zfill(2)
-            x = x.replace("xx", operand_address)
+                x = "0"
+        elif "xx" in x:
+            if operand:
+                if operand.token_type == TokenType.OPERAND_LITERAL:
+                    operand_address: str = str(operand.value).zfill(2)
+                else:
+                    operand_address = str(label_table[operand.value]).zfill(2)
+                x = x.replace("xx", operand_address)
+
         machine_code.append(x)
 
     return machine_code
